6736a02ca551d340b6a89bc4528872c4b6b5235d
Adam Koniuszewski <adam@imaginarydesign.co.uk>
2016-05-24 22:52:39 +0100

    collapse panels

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -14,7 +14,39 @@ function saveChecked(key, value){
 new Vue({
   el: '#app',
   data: {
-    completed: fetchChecked("project-check")
+    completed: fetchChecked("project-check"),
+    tasks: [
+      {
+        section: "Content",
+        items: [
+          {"name": "Spelling checked", "id": "spelling"},
+          {"name": "Placeholder content removed", "id": "placeholders"},
+          {"name": "Contact details correct", "id": "contact"},
+          {"name": "Logo links to index page", "id": "logo"},
+          {"name": "404 page exist", "id": "404"},
+          {"name": "All forms tested", "id": "forms"}, 
+          {"name": "Checked for broken links", "id": "links"}
+        ]
+      },
+      {
+        section: "Usability",
+        items: [
+          {"id": "screens", "name": "Check on screens"},
+          {"id": "spelling", "name": "Spelling"}
+        ]
+      },
+      {
+        section: "Design",
+        items: [
+          {"id": "consistency", "name": "Consistency"},
+          {"id": "colour", "name": "Colour"}
+        ]
+      }
+    ]
+  },
+
+  computed: {
+    // 
   },
 
   methods: {
@@ -29,4 +61,9 @@ new Vue({
     });
   }
 
+});
+
+// Collapse panels
+$('.btn-box-tool').click(function(){
+  $(this).find('span').toggleClass('glyphicon-minus glyphicon-plus');
 });
